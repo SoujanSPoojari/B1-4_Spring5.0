f49e49913d430537cc59c749c2d9714ddd3f2ed7
HP <HP@LAPTOP-7DRK1N1V>
2022-06-09 21:40:36 +0530

    First Commit

---
+++ SprinStarterProject1/src/main/java/com/example/entities/Student.java
@@ -0,0 +1,30 @@
+package com.example.entities;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+private int id;
+private String name;
+	
+	public Student() {
+		super();
+		System.out.println("Student");
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void display()
+	{
+		System.out.println("B1-4 Students");
+	}
+}
---
+++ SprinStarterProject1/src/test/java/com/example/springstarterproject1/SprinStarterProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springstarterproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinStarterProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
